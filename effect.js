5e8b257e9f461afa79237aafce88b712708abd4e
iotcat <i@iotcat.me>
2022-03-11 11:17:56 +0800

    word

--- effect.js
+++ effect.js
@@ -173,12 +173,13 @@ $('document').ready(function(){
 		var i;
 
 		function msgLoop (i) {
+
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i== $("#message_row").children("p").length+2){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('slow');
+			if(i== $("#message_row").children("p").length){
+				$("p:nth-child("+i+")").delay(4000).fadeOut('slow').promise().done(function () {
+					$('.cake').delay(400).fadeIn('slow');
 				});
 				
 			}
