847cffb8f2b2ede86a2f32fb4f07d4d1df2b80d4
iotcat <i@iotcat.me>
2022-03-11 11:41:37 +0800

    boll

--- effect.js
+++ effect.js
@@ -132,7 +132,7 @@ $('document').ready(function(){
 	});
 
 	$('#light_candle').click(function(){
-		$('.fuego').fadeIn(3000);
+		$('.fuego').fadeIn('slow');
 		$(this).fadeOut('slow').promise().done(function(){
 			$('#wish_message').fadeIn('slow');
 		});
@@ -150,13 +150,13 @@ $('document').ready(function(){
 		$('#b5').attr('id','b55')
 		$('#b6').attr('id','b66')
 		$('#b7').attr('id','b77')
-		$('#b11').animate({top:240, left: vw-350},500);
-		$('#b22').animate({top:240, left: vw-250},500);
-		$('#b33').animate({top:240, left: vw-150},500);
-		$('#b44').animate({top:240, left: vw-50},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+150},500);
-		$('#b77').animate({top:240, left: vw+250},500);
+		$('#b11').animate({top:240, left: vw-350},3000);
+		$('#b22').animate({top:240, left: vw-250},3000);
+		$('#b33').animate({top:240, left: vw-150},3000);
+		$('#b44').animate({top:240, left: vw-50},3000);
+		$('#b55').animate({top:240, left: vw+50},3000);
+		$('#b66').animate({top:240, left: vw+150},3000);
+		$('#b77').animate({top:240, left: vw+250},3000);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
